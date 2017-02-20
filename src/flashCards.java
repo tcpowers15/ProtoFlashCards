65a68a71579d3a85e40d2cf6069d84d8f102146a
tcp3256 <tcp3256@cs.rit.edu>
2017-02-19 20:26:36 -0500

    Initial commit

---
+++ src/flashCards.java
@@ -0,0 +1,11 @@
+/**
+ * Author: Trevor Powers
+ *
+ * This is the main file for the prototype flashcard program. This program will
+ * will have the functionality of the app, but will be limited to the command
+ * line, and a basic text based interface.
+ * The purpose of this is to flesh out the objects, and methods that will be
+ * needed.
+ */
+public class flashCards {
+}
